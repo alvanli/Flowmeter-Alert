5e816b3ec69ac9a822001ec5730df24ca36d2b42
f73chen <55363139+f73chen@users.noreply.github.com>
2021-07-09 17:43:39 -0400

    add separate variables for curr1 and curr2

--- NRF/Testing/Photoresistor/Photoresistor.ino
+++ NRF/Testing/Photoresistor/Photoresistor.ino
@@ -5,7 +5,8 @@
 RF24 radio(9, 10); // CE, CSN         
 const byte address[6] = "00001";
 
-bool curr = false; // true if below threshold, false if above threshold
+bool curr1 = false; // true if below threshold, false if above threshold
+bool curr2 = false;
 int threshold = 250;
 
 int photo1 = A0;
@@ -28,8 +29,20 @@ void loop(){
   Serial.println("Photo resistor 1: " + String(photoValue1));
   Serial.println("Photo resistor 2: " + String(photoValue2));
 
-  int val1 = threshold_check(photoValue1);
-  int val2 = threshold_check(photoValue2);
+  int val1 = threshold_check(photoValue1, curr1);
+  int val2 = threshold_check(photoValue2, curr2);
+  if (val1 == 1){
+    curr1 = true;
+  }
+  if (val1 == -1){
+    curr1 = false;
+  }
+  if (val2 == 1){
+    curr2 = true;
+  }
+  if (val2 == -1){
+    curr2 = false;
+  }
 
   //val1 = photoValue1;
   //val2 = photoValue2;
@@ -44,12 +57,10 @@ void loop(){
 // return 1 if reading moved below threshold (from bad to good)
 // return 0 if no change
 // return -1 if reading moved above threshold (from good to bad)
-int threshold_check(int reading) {
+int threshold_check(int reading, bool curr) {
   if (curr && (reading > threshold)) {
-    curr = false;
     return -1; 
   } else if (!curr && (reading < threshold)) {
-    curr = true;
     return 1;
   }
   return 0;
