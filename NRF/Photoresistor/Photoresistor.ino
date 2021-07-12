c24e087ae084372050d552f631fa543a8a3e5f2f
brian <b384li@uwaterloo.ca>
2021-07-12 16:28:55 -0400

    fix logic

--- NRF/Photoresistor/Photoresistor.ino
+++ NRF/Photoresistor/Photoresistor.ino
@@ -5,8 +5,8 @@
 RF24 radio(9, 10); // CE, CSN         
 const byte address[6] = "00001";
 
-bool curr1 = false; // true if below threshold, false if above threshold
-bool curr2 = false;
+bool blocked1 = false; // true if above threshold (blocked), false if below (not blocked)
+bool blocked2 = false;
 
 int photo1 = A0; // higher resistor
 int photo2 = A1; // lower resistor
@@ -47,19 +47,19 @@ void loop(){
   Serial.println("Photo resistor 1: " + String(photoValue1));
   Serial.println("Photo resistor 2: " + String(photoValue2));
 
-  int val1 = threshold_check(photoValue1, curr1, threshold1);
-  int val2 = threshold_check(photoValue2, curr2, threshold2);
+  int val1 = threshold_check(photoValue1, blocked1, threshold1);
+  int val2 = threshold_check(photoValue2, blocked2, threshold2);
   if (val1 == 1){
-    curr1 = true;
+    blocked1 = true;
   }
   if (val1 == -1){
-    curr1 = false;
+    blocked1 = false;
   }
   if (val2 == 1){
-    curr2 = true;
+    blocked2 = true;
   }
   if (val2 == -1){
-    curr2 = false;
+    blocked2 = false;
   }
 
   //val1 = photoValue1;
@@ -75,10 +75,10 @@ void loop(){
 // return -1 if reading moved below threshold (from bad to good)
 // return 0 if no change
 // return 1 if reading moved above threshold (from good to bad)
-int threshold_check(int reading, bool curr, int threshold) {
-  if (curr && (reading > threshold)) {
+int threshold_check(int reading, bool blocked, int threshold) {
+  if (!blocked && (reading > threshold)) {
     return 1; 
-  } else if (!curr && (reading < threshold)) {
+  } else if (blocked && (reading < threshold)) {
     return -1;
   }
   return 0;
