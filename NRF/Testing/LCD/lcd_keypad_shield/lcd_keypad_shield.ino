267572a479da13776f0afe4f1facfb4a17f53870
candaceng <candacewyng@gmail.com>
2021-07-12 15:53:55 -0400

    bug

--- NRF/Testing/LCD/lcd_keypad_shield/lcd_keypad_shield.ino
+++ NRF/Testing/LCD/lcd_keypad_shield/lcd_keypad_shield.ino
@@ -1,7 +1,5 @@
 #include <LiquidCrystal.h>
 #include "detectButton.h"
-#include "pitches.h"
-
 
 LiquidCrystal lcd(7, 6, 5, 4, 3, 2);
 
@@ -12,9 +10,7 @@ const String ALARM_STR = "Alarm";
 const String THRESHOLD_STR = "Set Threshold";
 const String LED_STR = "LED";
 const String RESET_STR = "RESET";
-const String BACK_STR = "BACK";
-const String CALI_STR = "CALIBRATE";
-const String MENU_ITEMS[] = {ALARM_STR, THRESHOLD_STR, LED_STR, RESET_STR, BACK_STR, CALI_STR};
+const String MENU_ITEMS[] = {ALARM_STR, THRESHOLD_STR, LED_STR, RESET_STR};
 const int MENU_SIZE = 4;
 int THRESHOLD = 1000;
 
@@ -30,9 +26,7 @@ void setup() {
 }
 
 int state = 0; // 0: Warning, 1: Menu, 2: Thres Menu
-int prevState = 0;
-int menuState = 0; // 0: ALARM_STR, 1: THRESHOLD_STR, 2: LED_STR, 3: RESET_STR, 4: BACK_STR, 5: CALI_STR;
-int prevMenuState = 0;
+int menuState = 2; // 0: ALARM_STR, 1: THRESHOLD_STR, 2: LED_STR
 boolean led_state = true;
 boolean alarm_state = true;
 
@@ -90,33 +84,18 @@ void loop() {
   //Serial.println(yPosition);
   delay(1000);
   if (state == 0){ 
-<<<<<<< HEAD:NRF/Testing/LCD/lcd_keypad_shield/lcd_keypad_shield.ino
     displayLCD(WARNING, "");
-    if (lcdKey == BTN_SELECT) state = 1;
+    if (SW_state == 0) state = 1;
   }
   else if (state == 1){ 
     mainMenuDisplay(menuState);
     if (yPosition < 100){
       menuState = menuState >= MENU_SIZE -1 ? 0 : menuState + 1;
     }else if (yPosition > 900){
-=======
-    display(WARNING, "");
-    if (lcdKey == BTN_SELECT){
-      state = 1;
-      prevState = 0;
-    }
-  }
-  else if (state == 1){ 
-    mainMenuDisplay(menuState);
-    if (lcdKey == BTN_DOWN){
-      menuState = menuState >= MENU_SIZE-1 ? 0 : menuState + 1;
-    }else if (lcdKey == BTN_UP){
->>>>>>> 0a8c6e4d4db0cc904712fb24cb97a73200462c26:NRF/Testing/LCD/lcd_keypad_shield.ino
       menuState = menuState == 0 ? MENU_SIZE-1 : menuState - 1;
     }else if (SW_state == 0){
       if (menuState == 1){
         state = 2;
-        prevState = 1;
       }else if (menuState == 2){
         led_state = !led_state;
         forceRefresh();
@@ -130,24 +109,13 @@ void loop() {
         forceRefresh();
         displayLCD("Reset", "Complete");
         delay(1000);
-        // send reset
-      }else if (menuState == 4){
-        state = 0;
-        prevState = 1;
-      }else if (menuState == 5){
-        // send cali
       }
     }
   }
   else if (state == 2) { 
-<<<<<<< HEAD:NRF/Testing/LCD/lcd_keypad_shield/lcd_keypad_shield.ino
     displayLCD(THRESHOLD_STR, String(THRESHOLD));
 
     if(yPosition < 100){
-=======
-    display(THRESHOLD_STR, String(THRESHOLD));
-    if(lcdKey == BTN_DOWN){
->>>>>>> 0a8c6e4d4db0cc904712fb24cb97a73200462c26:NRF/Testing/LCD/lcd_keypad_shield.ino
       THRESHOLD -= 5;
       forceRefresh();
     }else if (yPosition > 900){
@@ -155,7 +123,6 @@ void loop() {
       forceRefresh();
     }else if (SW_state == 0){
       state = 0;
-      prevState = 2;
     }
   }
   
