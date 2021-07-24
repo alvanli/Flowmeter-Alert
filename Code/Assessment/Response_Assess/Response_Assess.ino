3cf14f06fe1ff02a3ab7cd6b39b3316a36ecb064
brian <b384li@uwaterloo.ca>
2021-07-24 17:24:07 -0400

    user testing pt. 2

--- Code/Assessment/Response_Assess/Response_Assess.ino
+++ Code/Assessment/Response_Assess/Response_Assess.ino
@@ -1,11 +1,15 @@
 #include "C:/DATA/Git/Flowmeter-Alert/Code/library/lcd.h"
 
 int randVal = 0;
+bool awaitingInput = false;
+unsigned long timeStart = 0;
 
 void setup() {
+  Serial.begin(9600);
   randomSeed(analogRead(0)); // assuming pin 0 isn't used
   lcdInit();
   pinMode(speaker_pin, OUTPUT);
+  timeStart = millis();
 }
 
 void loop() {
@@ -16,12 +20,22 @@ void loop() {
       forceRefresh();
       playing = true;
       showFlash = true;
+      awaitingInput = true;
     }
   }
 
-  if (!playing && !showFlash && !warning_string.equals("NO WARNING")) { // reset message if needed
+  if (!playing && !showFlash && !warning_string.equals("NO WARNING")) { // reset if needed
     setWarningString("NO WARNING");
     forceRefresh();
+    awaitingInput = false;
+  }
+
+  if (Serial.available() > 0) { // output response time
+    Serial.println("Response time (ms): " + String(millis() - timeStart));
+  }
+
+  if (!awaitingInput) { // update time if not waiting for user input
+    timeStart = millis();
   }
   
   showMenuAlarmCheck(0);
