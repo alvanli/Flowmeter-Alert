652cd5495e068acb899ea40f5bb86d92d6c0ac4a
alvanli <51011489+alvanli@users.noreply.github.com>
2021-07-25 11:30:55 -0400

    added data for response time

--- Code/Assessment/Response_Assess/Response_Assess.ino
+++ Code/Assessment/Response_Assess/Response_Assess.ino
@@ -1,13 +1,17 @@
 #include "C:/DATA/Git/Flowmeter-Alert/Code/library/detectButton.h"
 #include "C:/DATA/Git/Flowmeter-Alert/Code/library/lcd.h"
 
-
 int randVal = 0;
 bool awaitingInput = false;
 unsigned long timeStart = 0;
 
 void setup() {
   Serial.begin(9600);
+  
+  pinMode(VRx, INPUT);
+  pinMode(VRy, INPUT);
+  pinMode(SW, INPUT_PULLUP); 
+  
   randomSeed(analogRead(0)); // assuming pin 0 isn't used
   lcdInit();
   pinMode(speaker_pin, OUTPUT);
@@ -20,29 +24,32 @@ void loop() {
   lcdKey = readLCDButtons();
   
   if (!showFlash && !playing && !awaitingInput) { // play alarm randomly
-    randVal = random(1000);
-    if (randVal < 2) {
-      setWarningString("TOO HIGH");
-      forceRefresh();
-      playing = true;
-      showFlash = true;
-      awaitingInput = true;
-    }
+    randVal = random(2, 10);
+    Serial.println(randVal*3);
+    setWarningString("NO WARNING");
+    forceRefresh();
+    delay(randVal * 1000 * 3);
+    timeStart = millis();
+    setWarningString("TOO HIGH");
+    forceRefresh();
+    playing = true;
+    showFlash = true;
+    awaitingInput = true;
   }
 
   if (!playing && !showFlash && !warning_string.equals("NO WARNING") && !awaitingInput) { // reset if needed
     setWarningString("NO WARNING");
     forceRefresh();
   }
-  Serial.println(lcdKey);
-  if (lcdKey == BTN_UP) { // output response time
-    //Serial.println("Response time (ms): " + String(millis() - timeStart));
+
+  if (lcdKey == BTN_SELECT) { // output response time
+    Serial.println("Response time (ms): " + String(millis() - timeStart));
     awaitingInput = false;
+    setWarningString("NO WARNING");
+    forceRefresh();
   }
 
-  if (!awaitingInput) { // update time if not waiting for user input
-    timeStart = millis();
-  }
   
   showMenuAlarmCheck(0);
+  delay(1);
 }
